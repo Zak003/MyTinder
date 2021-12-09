a789dd41ecf5045f9566e50150e77e81a7062c5c
Zak003 <59819711+Zak003@users.noreply.github.com>
2021-12-09 18:47:30 +0100

    postavitev in registracija

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
+
 environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
     Popper: ['popper.js', 'default']
   })
 )
+
+environment.config.set('resolve.alias', {jquery: 'jquery/src/jquery'}); //#jquery ki smo ga napisali odzgoraj smo v tej vrstici povedali tocno pot do jquery
 
 
 module.exports = environment
