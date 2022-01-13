0f1260ddbf3b5aeff9b83c17bfd03a61de1de432
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-01-13 20:37:45 +0100

    browse link dizajn in brskanje oseb

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'webpacker', '~> 5.0'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+gem "aws-sdk-s3", require: false
+
 gem "devise"
 
 # Reduces boot times through caching; required in config/boot.rb
@@ -46,3 +48,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'font_awesome5_rails'
---
+++ app/javascript/includes/slides.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    var $activeSlide = $('#slides .slide:first-child');
+
+    //show first slide
+    $activeSlide.addClass("showing");
+
+    $("#decline").on("click", function(){
+        goToSlide('decline');
+    });
+
+    $("#approve").on("click", function(){
+        goToSlide('approve');
+    });
+
+    function goToSlide(action) {
+        $activeSlide.removeClass("showing");
+        $activeSlide = $activeSlide.next(".slide");
+
+        //send data to controller
+        if(action == "approve"){
+            console.log(action);    //izpiše v konzoli approve 
+        }else{
+            console.log(action);    //izpiše v konzoli decline 
+        }
+
+        $activeSlide.addClass("showing");
+    }
+});
