329f7efec7123ab3f5c9e9ad161c7a492c9df72f
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-01-20 17:56:05 +0100

    naj bi delo upload to amazon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :configure_devise, if: :devise_controller?
 
-    private
+    protected
 
     def configure_devise
-        devise_parameter_sanitizer.permit(:account_update,  keys: [ :first_name, :last_name, :username, :image ]) 
+        devise_parameter_sanitizer.permit(:account_sign_up, keys: [:first_name, :last_name, :username, :image])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :image]) 
     end
     
 end
