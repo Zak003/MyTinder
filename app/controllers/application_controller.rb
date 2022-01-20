0dd62ae205efad899269b14de0165969e14ab906
Zak003 <59819711+Zak003@users.noreply.github.com>
2022-01-20 20:34:03 +0100

    dodajanje slik tudi na sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_devise
-        devise_parameter_sanitizer.permit(:account_sign_up, keys: [:first_name, :last_name, :username, :image])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :image])
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :image]) 
     end
     
